d2d4b3ebaca753e18c68cfecdb95b482daa8548f
Haley <36253327+haleybear@users.noreply.github.com>
2024-07-18 23:18:04 -0700

    Scrolling Stack of Desserts
    Working:
    -Scrolling nav stack of dessert titles and their thumbnails that lead to a screen with the recipe title
    - The stack is already in alphabetical order from the API (this is something that still needs to be sanity checked)
    In Progress -
    Figuring out best method of parsing the multiple ingredients and measurements and adding them to a dictionary before displaying them on their related screens.

--- recipeRecall/recipeRecall/ContentView.swift
+++ recipeRecall/recipeRecall/ContentView.swift
@@ -5,31 +5,80 @@
 //  Created by Haley Bear on 7/18/24.
 //
 
+// for the sake of timing - lock the view unless you get everything else done first
+
 import SwiftUI
 
 var recipeTitle = "Yummy Dessert Name"
 
-struct Recipe{
-    let title: String
-    let thumbnail: String
-}
-
 struct ContentView: View {
-       
+    @State private var meals = [Meal]()
+    @State private var recipes = [Recipe]()
+    //let recipe: Recipe
+    // #DETAILSELECTION @State var selection = ClickableRecipeCard*
+    // *whether the card is expandable or transitions to new screen
     var body: some View {
-        HStack {
-            RoundedRectangle(cornerRadius: 25.0)
-                .fill(.blue)
-                .frame(width: 100, height:100)
-                .accessibilityLabel(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/)
-            VStack(alignment: .leading){
-                Text(recipeTitle)
+        //main menu list view item
+        
+        NavigationStack{
+            List(meals) { meal in
+                HStack {
+                    AsyncImage(url: URL(string: meal.strMealThumb + "/preview")){ phase in switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: 100, maxHeight: 100)
+                                .cornerRadius(25.0)
+                                .accessibilityLabel("Image preview of \(meal.strMeal)")
+                        case .failure:
+                            Image(systemName: "photo")
+                        @unknown default:
+                            EmptyView()
+                            }
+                    }
+                    Spacer().frame(width: 4)
+                    NavigationLink(meal.strMeal){
+                        VStack(alignment: .leading){
+                            Text(meal.strMeal)
+                                .font(.headline)
+                            }
+                        }
+                    }
+                }
+        }
+        .task{
+            do{
+                meals = try await fetchDesserts()
+//                List(meals){ recipe in
+//                   recipe = try await fetchDesserts( recipe.idMeal )
+//                }
+                print("MealResponse")
+            } catch MealError.invalidURL{
+                print("Invalid Url")
+            } catch MealError.invalidData{
+                print("Invalid Data")
+            } catch MealError.invalidResponse{
+                print("Invalid Response")
+            } catch {
+                print("Unknown Error - our apologies")
             }
         }
-        .padding()
+        
+        // detail view to be moved to separate screen/ or expand and contract
+        // Picker("Select Recipe", selection: $selection){
+        //    ForEach(recipe.allCases, id:\.self)
+        //}
+        // .pickerStyle(.segmented)
+        // async api call here
+        // Recipe?.ingredient ?? "Placeholder Ingredient
+        
+        //On selection action .task{fetchRecipeDetails( recipeID )}
     }
 }
 
+
 #Preview {
-    ContentView()
+  ContentView()
 }
--- recipeRecall/recipeRecall/recipeRecallApp.swift
+++ recipeRecall/recipeRecall/recipeRecallApp.swift
@@ -11,20 +11,78 @@ import SwiftUI
 let apiKey = 1
 
 
-//func fetchDessertsFromAPI() async -> [Meal] {
-//    let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert?api_key=\(apiKey)")!
-//    let (data, _) = try await URLSession.shared.data(from: url)
-//    let decoded = try JSONDecoder().decode(MealResponse.self: from:data)
-//}
-//
-//struct MealResponse: Decodable {
-//    let results: [Meal]
-//}
-//struct Meal: Decodable {
-//    let strMeal: String
-//    let strMealThumb: String
-//    let idMeal: Int
-//}
+enum MealError : Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
+
+//Get the data for all desserts
+struct MealResponse: Decodable {
+    let meals: [Meal]
+}
+
+//Break the desserts down by meal?
+struct Meal: Decodable, Identifiable {
+    var id: String {idMeal}
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String
+}
+
+struct RecipeResponse: Decodable {
+    let meals: [Recipe]
+}
+
+struct Recipe: Decodable, Identifiable {
+    var id: String {idMeal}
+    let idMeal: String
+    let title: String
+    let thumbnail: String
+    let recipeInstructions: String
+    //To-Do - Ingredients and Measurements will need to either be pre-compiled and displayed or....something else
+    var ingredient: String
+    var measurement: String
+}
+
+// Fetch desserts
+
+func fetchDesserts() async throws -> [Meal] {
+    guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else { throw MealError.invalidURL }
+    let (data, response) = try await URLSession.shared.data(from: url)
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+        throw MealError.invalidResponse
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(MealResponse.self, from: data).meals
+    } catch {
+        throw MealError.invalidData
+    }
+}
+
+func fetchRecipeDetails( recipeID : Meal ) async throws -> [Recipe] {
+    let recipeID = recipeID
+    guard let url = URL(string: "https://themealdb.com/api/json/v1/1/lookup.php?i=\(recipeID)") else { throw MealError.invalidURL }
+    let (data, response) = try await URLSession.shared.data(from: url)
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+        throw MealError.invalidResponse
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(RecipeResponse.self, from: data).meals
+    } catch {
+        throw MealError.invalidData
+    }
+}
+
+
+// Sort function - in the api call return preview the desserts are already in alphabetical order. Is it preferable to trust that they will remain that way or to add extra functionality to check this and potentially resort them?
+
+// build a test where the alphabet is Not correct and see that this sort is only called when necessary (have a sanity test for all calls, but only call sort when needed)
+
 
 @main
 struct recipeRecallApp: App {
